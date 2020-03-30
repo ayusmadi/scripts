ccf6c791896d540266e49c133fca0294965a0aa3
ayusmadi <57968900+ayusmadi@users.noreply.github.com>
2020-03-30 20:20:30 +0800

    Update main.sh

--- azure/Update or rotate the credentials for Azure Kubernetes Service (AKS)/main.sh
+++ azure/Update or rotate the credentials for Azure Kubernetes Service (AKS)/main.sh
@@ -10,15 +10,14 @@ SP_ID=$(az aks show --resource-group $myResourceGroup --name $myAKSCluster \
 # THIS CHANGE SECRET OF EXISTING SERVICE PRINCIPAL
 # YOU NEED TO UPDATE THIS SECRET IN OTHER PLACES IF IT IS USED FOR AUTOMATION
 # SP_SECRET=$(az ad sp credential reset --name $SP_ID --query password -o tsv)
+# echo $SP_SECRET > $SP_ID.secret
 
+# This just update the secret
 echo -n "Enter the new SP secret:"
 read SP_SECRET
 
-az aks update-credentials \
-    --resource-group $myResourceGroup 
-    
-    echo -e "Enter new secret:"
-    read SP_SECRET
+az aks update-credentials --no-wait \
+    --resource-group $myResourceGroup \
     --name $myAKSCluster \
     --reset-service-principal \
     --service-principal $SP_ID \
